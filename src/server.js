f24f472a00a8decc348b67e07c384aba0e1dc43f
whitebear05 <woojs0505@naver.com>
2021-09-07 22:22:43 +0900

    update

--- src/server.js
+++ src/server.js
@@ -5,33 +5,37 @@ import Food from "./models/Food";
 const app = express();
 const logger = morgan("dev");
 
-app.use(express.json());
 app.use(logger);
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/barcodes/:id", async (req, res) => {
-  const id = req.params.id;
-  const food = await Food.findOne({ barcodeNumber: id }).exec();
+// 바코드 번호로 음식 조회, 객체 반환
+app.get("/barcodes/:barcodenum", async (req, res) => {
+  const barcodenum = req.params.barcodenum;
+  const food = await Food.findOne({ barcodeNumber: barcodenum }).exec();
   if (!food) {
     return res.status(404).json({ error: "Not found.", status: 404 });
   }
-  const foodID = food._id;
   return res.status(200).json({
-    foodID: foodID,
+    food,
     status: 200,
   });
 });
 
-app.get("/foods/:id([0-9a-f]{24})", async (req, res) => {
-  const id = req.params.id;
-  const food = await Food.findById(id);
+// 음식 이름으로 음식 조회, 객체 반환
+app.get("/search/:foodname", async (req, res) => {
+  const foodname = decodeURI(req.params.id);
+  const food = await Food.findOne({ foodName: foodname }).exec();
   if (!food) {
     return res.status(404).json({ error: "Not found.", status: 404 });
   }
-  // const json = JSON.stringify(food);
-  return res.status(200).json({ food, status: 200 });
+  return res.status(200).json({
+    food,
+    status: 200,
+  });
 });
 
+// 음식 등록
 app.post("/foods", async (req, res) => {
   const {
     foodName,
@@ -59,6 +63,7 @@ app.post("/foods", async (req, res) => {
       message: "Succeed to add new food",
     });
   } catch (error) {
+    console.log(error);
     return res.status(400).json({
       status: 400,
       error: "Registration failed, Please try again.",
