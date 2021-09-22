a4d402443a3a0778ff6a18b1f67dd6e9d622200b
whitebear <woojs0505@naver.com>
2021-09-22 14:14:27 +0900

    Fix: Food model barcodeNumber error

--- src/models/Food.js
+++ src/models/Food.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 
 const foodSchema = new mongoose.Schema({
-  foodName: { type: String, required: true },
+  foodName: { type: String, required: true, unique: true },
   safetyLevel: { type: Number, required: true },
   safetyGrade: { type: String, required: true },
   edible: { type: String, required: true },
   symptom: [{ type: String }],
   feedMethod: [{ type: String }],
   ingredient: [{ type: String }],
-  barcodeNumber: [{ type: Number, required: true, unique: true }],
+  barcodeNumber: [{ type: Number, required: true }],
 });
 
 const Food = mongoose.model("Food", foodSchema);
