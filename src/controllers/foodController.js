919ed1f4d7ed4c0c37a2e8893be239d089082d4b
whitebear <woojs0505@naver.com>
2021-09-22 12:58:33 +0900

    Update checking redundancy

--- src/controllers/foodController.js
+++ src/controllers/foodController.js
@@ -42,6 +42,29 @@ export const postFood = async (req, res) => {
       message: "Post Password is incorrect.",
     });
   }
+  const food = await Food.findOne({ foodName });
+  if (food) {
+    food.foodName = foodName;
+    food.safetyLevel = safetyLevel;
+    food.safetyGrade = safetyGrade;
+    food.edible = edible;
+    food.symptom = symptom;
+    food.feedMethod = feedMethod;
+    food.ingredient = ingredient;
+    food.barcodeNumber = barcodeNumber;
+    await food.save();
+    return res.status(200).json({
+      status: 200,
+      message: "Succeed to update a food",
+    });
+  }
+  const barcodeExists = await Food.exists({ barcodeNumber });
+  if (barcodeExists) {
+    return res.status(400).json({
+      status: 400,
+      error: "Barcode Number is already in use.",
+    });
+  }
   try {
     await Food.create({
       foodName,
